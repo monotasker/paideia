9082a16afda4e6eb395471afaf7a506950523733
Ian W. Scott <iscott@tyndale.ca>
2011-10-12 15:08:18 -0400

    "Fixed a few bugs and added the start of a functioning blog system for the front-page news and updates"

--- controllers/listing.py
+++ controllers/listing.py
@@ -5,23 +5,31 @@ def question():
     questions = db().select(db.questions.ALL, orderby = db.questions.question)
     return dict(questions = questions)
 
-@auth.requires_membership(role='administrators')    
+@auth.requires_membership(role='administrators')
 def quiz():
     quizzes = db().select(db.quizzes.ALL, orderby = db.quizzes.quiz)
     return dict(quizzes = quizzes)
 
-@auth.requires_membership(role='administrators')    
+@auth.requires_membership(role='administrators')
 def user():
     users = db().select(db.auth_user.ALL, orderby = db.auth_user.last_name)
     return dict(users = users)
 
-@auth.requires_membership(role='administrators')    
+@auth.requires_membership(role='administrators')
 def tags():
     tags = db().select(db.tags.ALL, orderby = db.tags.tag)
     return dict(tags = tags)
 
-@auth.requires_membership(role='administrators')    
+@auth.requires_membership(role='administrators')
 def bug():
-	the_q = request.args[0]
-	bugs = db(db.q_bugs.question == the_q).select()
-	return dict(bugs = bugs)
+    the_q = request.args[0]
+    bugs = db(db.q_bugs.question == the_q).select()
+    return dict(bugs = bugs)
+
+def news():
+    newslist = db(db.news).select()
+    if db((db.auth_membership.user_id == auth.user_id) & (db.auth_membership.group_id == 1)).select():
+        button = A('new story', _href=URL('creating', 'story.load'), cid='modal_frame')
+    else:
+        button = ''
+    return dict(newslist = newslist, button = button)
