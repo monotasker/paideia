4e76c2ff321ce8db534daae0d9550804cb53140d
Ian W. Scott <iscott@tyndale.ca>
2011-10-22 23:19:19 -0400

    "Fixed display of user's average question score in user report"

--- controllers/reporting.py
+++ controllers/reporting.py
@@ -2,18 +2,23 @@
 # try something like
 @auth.requires_membership(role='administrators')
 def index():
-  reports = dict(attempts='Attemps Log',)
-  return dict(reports=reports)
+    reports = dict(attempts='Attemps Log',)
+    return dict(reports=reports)
 
 def attempts():
-  form = SQLFORM.grid(db.attempt_log)
-  return dict(form = form)
+    form = SQLFORM.grid(db.attempt_log)
+    return dict(form = form)
 
 def user():
-  attempts = db(db.attempt_log.name == request.args[0]).select()
-  s=db.attempt_log.score.sum()
-  row = db(db.attempt_log.name == request.args[0]).select(s).first()
-  answer = row[s]
-  score_avg = answer/len(attempts)
-  form = SQLFORM.grid(db.attempt_log.name == request.args[0])
-  return dict(form=form, score_avg=score_avg)
+    the_user = db.auth_user[request.args[0]]
+    the_name = the_user.last_name + ', ' + the_user.first_name
+    try:
+        attempts = db(db.attempt_log.name == request.args[0]).select()
+        s=db.attempt_log.score.sum()
+        row = db(db.attempt_log.name == request.args[0]).select(s).first()
+        answer = row[s]
+        score_avg = answer/len(attempts)
+    except:
+        score_avg = "Can't calculate average"
+    form = SQLFORM.grid(db.attempt_log.name == request.args[0])
+    return dict(form=form, score_avg=score_avg, the_name = the_name)
