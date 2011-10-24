512891a068a759b1a31012d9476c55e2bca76d3b
Ian W. Scott <iscott@tyndale.ca>
2011-10-24 16:54:46 -0400

    "Adjustments integrating newer database content after merge problems with the fluxflex deployment repo"

--- models/db.py
+++ models/db.py
@@ -14,7 +14,7 @@ if request.env.web2py_runtime_gae:            # if running on Google App Engine
     # from google.appengine.api.memcache import Client
     # session.connect(request, response, db = MEMDB(Client()))
 else:                                         # else use a normal relational database
-    db = DAL('sqlite://storage.sqlite')       # if not, use SQLite or other DB
+    db = DAL('sqlite://storage.sqlite', migrate=True)       # if not, use SQLite or other DB
 
 # by default give a view/generic.extension to all actions from localhost
 # none otherwise. a pattern can be 'controller/function.extension'
