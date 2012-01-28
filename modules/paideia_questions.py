48ed66b9057b20d014982429e2c70de6d4b11cf0
Ian W. Scott <iscott@tyndale.ca>
2012-01-27 19:56:09 -0500

    Finished implementing tracking of tag-based statistics in models/paideia_questions.py; Also implemented border image in theme_paideia.less

--- modules/paideia_questions.py
+++ modules/paideia_questions.py
@@ -10,11 +10,14 @@ class question:
     def __init__(self):
         self.the_q = '' #the row object holding the selected question
         self.quiz_type = '' #the kind of algorithm used to select the question
-        self.rightDate = ''
-        self.wrongDate = ''
-        self.rightCount = ''
-        self.wrongCount = ''
-        self.score = ''
+        self.rightCount = 0
+        self.wrongCount = 0
+        self.score = 0
+        self.newTimesR = {}
+        self.newTimesW = {}
+        self.last_right = {}
+        self.last_wrong = {}
+        self.cat = {}
 
     def selectq(self):
         """
@@ -92,7 +95,6 @@ class question:
             question_index = random.randint(0, question_count)
             question_obj = questions[question_index].questions
 
-        print question_obj.id
         session.qID = question_obj.id
         session.question_text = question_obj.question
         session.answer = question_obj.answer
@@ -101,65 +103,138 @@ class question:
         session.readable_answer = question_obj.readable_answer
 
     def evalq(self):
-
+        # get web2py session objects
         session, db, auth = current.session, current.db, current.auth
-
-        print session.qID
-
+        # get the student's response to the question
         the_response = string.strip(session.response)
-        self.the_q = db(db.question_records.question==session.qID).select().first()
-
+        # retrieve the question data from the db to use in evaluating the answer
+        self.the_q = db(db.question_records.question == session.qID).select().first()
+        # compare the student's response to the regular expressions
         try:
             if re.match(session.answer, the_response, re.I):
                 session.eval = 'correct'
                 self.rightCount = 1
-                self.wrongCount = 0
-                if self.the_q:
-                    self.wrongDate = self.the_q.tlast_wrong
-                else:
-                    self.wrongDate = datetime.datetime.utcnow()
-                self.rightDate = datetime.datetime.utcnow()
                 self.score = 1
             elif re.match(session.answer2,session.response) and session.answer2 != 'null':
                 session.eval = 'partial'
-                self.rightCount = 0
-                self.wrongCount = 0
-                if self.the_q:
-                    self.wrongDate = self.the_q.tlast_wrong
-                    self.rightDate = self.the_q.tlast_right
-                else:
-                    self.wrongDate = datetime.datetime.utcnow()
-                    self.rightDate = datetime.datetime.utcnow()
                 self.score = 0.5
+                #TODO: Get this score value from the db instead of hard coding it here.
             elif re.match(session.answer3,session.response) and session.answer3 != 'null':
                 session.eval = 'partial'
-                self.rightCount = 0
-                self.wrongCount = 0
-                if self.the_q:
-                    self.wrongDate = self.the_q.tlast_wrong
-                    self.rightDate = self.the_q.tlast_right
-                else:
-                    self.wrongDate = datetime.datetime.utcnow()
-                    self.rightDate = datetime.datetime.utcnow()
                 #TODO: Get this score value from the db instead of hard coding it here.
                 self.score = 0.3
             else:
                 session.eval = "wrong"
                 self.wrongCount = 1
-                self.rightCount = 0
-                if self.the_q:
-                    self.rightDate = self.the_q.tlast_right
-                else:
-                    self.rightDate = datetime.datetime.utcnow()
-                self.wrongDate = datetime.datetime.utcnow()
-                self.score = 0
-
-            self.recordq()
+            # record the results in statistics for this question
+            self.recordq(session.qID)
+            # record the results in statistics for this question's tags
             self.recordtag(session.qID)
-
-        #handle errors if the response cannot be evaluated
+        #handle errors if the student's response cannot be evaluated
         except re.error:
             redirect(URL('index', args=['error', 'regex']))
+
+    def recordq(self, qID):
+        """
+        update or create database record for this question after the attempt is evaluated.
+        """
+        # get web2py session objects
+        session, db, auth = current.session, current.db, current.auth
+        #If the user has already attempted this question once, update their record for this question
+        qrecord = db((db.question_records.name == auth.user_id) & (db.question_records.question == qID)).select().first()
+        if qrecord:
+            indx = 'q%s' % qID
+            #PRINT - for debugging purposes
+            print indx
+            #calculate updated performance statistics
+            self.stats(qrecord)
+            #update the db record
+            #PRINT - for debugging purposes
+            print '\n\n', 'question (update)', qID, 'times_right', self.newTimesR[indx], 'times_wrong', self.newTimesW[indx], 'tlast_right', self.last_right[indx], 'tlast_wrong', self.last_wrong[indx], 'cat', self.cat[indx], '\n'
+            db(db.question_records.question == qID).update(
+                times_right = self.newTimesR[indx],
+                times_wrong = self.newTimesW[indx],
+                tlast_right = self.last_right[indx],
+                tlast_wrong = self.last_wrong[indx],
+                category = self.cat[indx])
+        #if the user hasn't attempted this question, create a new record for it
+        else:
+            #PRINT - for debugging purposes
+            print '\n\n', 'question (new)', qID, 'times_right', self.rightCount, 'times_wrong', self.wrongCount, 'cat', 1, '\n'
+            db.question_records.insert(
+                question = qID,
+                times_right = self.rightCount,
+                times_wrong = self.wrongCount,
+                category = 1)
+
+    def recordtag(self, qID):
+        """
+        update or create database record for this question after the attempt is evaluated.
+        """
+        #get web2py objects from current
+        session, db, auth = current.session, current.db, current.auth
+        # calculate and record stats for each tag attached to this question
+        tags = db(db.questions.id == qID).select(db.questions.tags).first()
+        for k, v in tags.items():
+            for tag in v:
+                # select record for this user on this tag
+                trecord = db((db.tag_records.name == auth.user_id)&(db.tag_records.tag == tag)).select().first()
+                # if the user has already tried this tag, update the record
+                if trecord:
+                    indx = 't%s' % tag
+                    #calculate updated performance statistics
+                    self.stats(trecord)
+                    #update the db record
+                    #PRINT - for debugging purposes
+                    print 'tag (update)', tag, 'times_right', self.newTimesR[indx], 'times_wrong', self.newTimesW[indx], 'tlast_right', self.last_right[indx], 'tlast_wrong', self.last_wrong[indx], 'cat', self.cat[indx], '\n'
+                    db(db.tag_records.tag==tag).update(
+                        times_right = self.newTimesR[indx],
+                        times_wrong = self.newTimesW[indx],
+                        tlast_right = self.last_right[indx],
+                        tlast_wrong = self.last_wrong[indx],
+                        category = self.cat[indx])
+                # if this is the user's first time for this tag, create a new record
+                else:
+                    #PRINT - for debugging purposes
+                    print 'tag (new)', tag, 'times_right', self.rightCount, 'times_wrong', self.wrongCount, 'category', 1, '\n'
+                    db.tag_records.insert(
+                        tag = tag,
+                        times_right = self.rightCount,
+                        times_wrong = self.wrongCount,
+                        category = 1)
+
+    def stats(self, record):
+        #figure out right and wrong counts for this q or tag
+        if record.has_key('tag'):
+            indx = 't%s' % record.tag
+            #PRINT - for debugging purposes
+            print indx
+        else:
+            indx = 'q%s' % record.question
+            #PRINT - for debugging purposes
+            print indx
+        #set times right and times wrong
+        timesR = record.times_right or 0
+        timesW = record.times_wrong or 0
+        self.newTimesR[indx] = int(timesR) + int(self.rightCount)
+        self.newTimesW[indx] = int(timesW) + int(self.wrongCount)
+        #set last_right and last_wrong
+        now_date = datetime.datetime.utcnow()
+        if self.rightCount == 1:
+            self.last_right[indx] = now_date
+            self.last_wrong[indx] = record.tlast_wrong or now_date
+        elif (self.rightCount == 0) and (self.wrongCount == 0):
+            self.last_right[indx] = record.tlast_right or now_date
+            self.last_wrong[indx] = record.tlast_wrong or now_date
+        else:
+            self.last_right[indx] = record.tlast_right or now_date
+            self.wrongDate = now_date
+        #figure out time-based stats for this q or tag
+        right_dur = now_date - self.last_right[indx]
+        wrong_dur = now_date - self.last_wrong[indx]
+        rightWrong_dur = self.last_right[indx] - self.last_wrong[indx]
+        #categorize q or tag based on this performance
+        self.cat[indx] = self.categorize(right_dur, wrong_dur, rightWrong_dur)
 
     def categorize(self, right_dur, wrong_dur, rightWrong_dur):
         """
@@ -175,68 +250,4 @@ class question:
                 cat = 2
         else:
             cat = 1
-
         return cat
-
-    def recordq(self):
-        """
-        update or create database record for this question after the attempt is evaluated.
-        """
-        session, db, auth = current.session, current.db, current.auth
-        #If the user has already attempted this question once, update their record for this question
-        if db((db.question_records.name==auth.user_id)&(db.question_records.question==session.qID)).select():
-            timesR = self.the_q.times_right
-            timesW = self.the_q.times_wrong
-            newTimesR = int(timesR) + int(self.rightCount)
-            newTimesW = int(timesW) + int(self.wrongCount)
-            last_right = self.rightDate
-            last_wrong = self.wrongDate
-            #figure out how the student is doing with this question
-            now_date = datetime.datetime.utcnow()
-            right_dur = now_date-last_right
-            wrong_dur = now_date-last_wrong
-            rightWrong_dur = last_right - last_wrong
-            #categorize this question based on student's performance
-            cat = self.categorize(now_date, last_right, last_wrong, right_dur, wrong_dur, rightWrong_dur)
-
-            #update the db record
-            db(db.question_records.question==session.qID).update(times_right=newTimesR, times_wrong=newTimesW, tlast_right=last_right, tlast_wrong=last_wrong, category=cat)
-        #if the user hasn't attempted this question, create a new record for it
-        else:
-            db.question_records.insert(question=session.qID, times_right=self.rightCount, times_wrong=self.wrongCount)
-
-    def recordtag(self, qID):
-        """
-        update or create database record for this question after the attempt is evaluated.
-        """
-
-        #get web2py objects from current
-        session, db, auth = current.session, current.db, current.auth
-
-        tags = db(db.questions.id == session.qID).select(db.questions.tags).first()
-
-        for k, v in tags.items():
-            for tag in v:
-                print 'tag # %s recorded in db.tag_records' % tag
-                # select record for this user on this tag
-                trecord = db((db.tag_records.name==auth.user_id)&(db.tag_records.tag==tag)).select()
-                # if the user has already tried this tag, update the record
-                if trecord:
-                    timesR = trecord.times_right
-                    timesW = trecord.times_wrong
-                    newTimesR = int(timesR) + int(self.rightCount)
-                    newTimesW = int(timesW) + int(self.wrongCount)
-                    #figure out time-based stats for this tag
-                    last_right = trecord.last_right
-                    last_wrong = trecord.last_wrong
-                    now_date = datetime.datetime.utcnow()
-                    right_dur = now_date-last_right
-                    wrong_dur = now_date-last_wrong
-                    rightWrong_dur = last_right - last_wrong
-                    #categorize tag based on this performance
-                    cat = self.categorize(right_dur, wrong_dur, rightWrong_dur)
-                    #update the db record
-                    db(db.question_records.question==session.qID).update(times_right=newTimesR, times_wrong=newTimesW) #tlast_right=last_right, tlast_wrong=last_wrong, category=cat
-                # if this is the user's first time for this tag, create a new record
-                else:
-                    db.tag_records.insert(tag=tag, times_right=self.rightCount, times_wrong=self.wrongCount)
