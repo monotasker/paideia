032a37dd67a6ff8c4714305fae2f9f734a06461c
Ian W. Scott <iscott@tyndale.ca>
2012-02-07 18:06:10 -0500

    Bug killing in plugin_ajaxselect multiselect option.

--- models/paideia.py
+++ models/paideia.py
@@ -8,12 +8,11 @@ if 0:
     auth = Auth()
     db = DAL()
     request = current.request
-    from applications.paideia.modules.plugin_multiselect_widget import hmultiselect_widget, vmultiselect_widget
+    from applications.paideia.modules.plugin_ajaxselect import AjaxSelect
 
 #os module needed for setting upload folders for images and audio
 import os
 #plugin from http://dev.s-cubism.com/plugin_multiselect_widget
-from plugin_multiselect_widget import hmultiselect_widget, vmultiselect_widget
 from plugin_ajaxselect import AjaxSelect
 import datetime
 
@@ -22,48 +21,48 @@ dtnow = datetime.datetime.utcnow()
 db.define_table('categories',
     Field('category'),
     Field ('description'),
-    format='%(category)s')
+    format = '%(category)s')
 
 db.define_table('tags',
     Field('tag', 'string'),
-    format='%(tag)s')
+    format = '%(tag)s')
 
 db.define_table('locations',
     Field('location'),
-    Field('background', 'upload', uploadfolder=os.path.join(request.folder, "static/images")),
-    format='%(location)s')
+    Field('background', 'upload', uploadfolder = os.path.join(request.folder, "static/images")),
+    format = '%(location)s')
 
 db.define_table('npcs',
     Field('name', 'string'),
     Field('location', 'list:reference db.locations'),
-    Field('image', 'upload', uploadfolder=os.path.join(request.folder,"static/images")),
-    format='%(name)s')
+    Field('image', 'upload', uploadfolder = os.path.join(request.folder, "static/images")),
+    format = '%(name)s')
 
 db.define_table('questions',
     Field('question', 'text'),
     Field('answer'),
-    Field('value', 'double', default=1.0),
+    Field('value', 'double', default = 1.0),
     Field('readable_answer'),
-    Field('answer2', default='null'),
-    Field('value2', 'double', default=0.5),
-    Field('answer3', default='null'),
-    Field('value3', 'double', default=0.3),
+    Field('answer2', default = 'null'),
+    Field('value2', 'double', default = 0.5),
+    Field('answer3', default = 'null'),
+    Field('value3', 'double', default = 0.3),
     Field('frequency', 'double'),
     Field('tags', 'list:reference db.tags'),
     Field('tags_secondary', 'list:reference db.tags'),
     Field('status', 'integer'),
     Field('npcs', 'list:reference db.npcs'),
     Field('next', 'list:reference db.questions'),
-    Field('audio', 'upload', uploadfolder=os.path.join(request.folder, "static/audio")),
-    format='%(question)s')
+    Field('audio', 'upload', uploadfolder = os.path.join(request.folder, "static/audio")),
+    format = '%(question)s')
 
 db.questions.tags.requires = IS_IN_DB(db, 'questions.id', db.questions._format, multiple = True)
 db.questions.npcs.requires = IS_IN_DB(db, 'npcs.id', db.npcs._format, multiple = True)
-db.questions.npcs.widget = lambda field, value: AjaxSelect(field, value, 'tags', multi = 'v').widget()
+db.questions.npcs.widget = lambda field, value: AjaxSelect(field, value, 'npcs', multi = 'basic').widget()
 db.questions.tags.requires = IS_IN_DB(db, 'tags.id', db.tags._format, multiple = True)
-db.questions.tags.widget = lambda field, value: AjaxSelect(field, value, 'tags', refresher = True, multi = 'v').widget() #vmultiselect_widget
+db.questions.tags.widget = lambda field, value: AjaxSelect(field, value, 'tags', refresher = True, multi = 'basic').widget()
 db.questions.tags_secondary.requires = IS_IN_DB(db, 'tags.id', db.tags._format, multiple = True)
-db.questions.tags_secondary.widget = lambda field, value: AjaxSelect(field, value, 'tags', multi = 'v').widget()
+db.questions.tags_secondary.widget = lambda field, value: AjaxSelect(field, value, 'tags', multi = 'basic').widget()
 
 db.define_table('quizzes',
     Field('quiz'),
@@ -71,67 +70,67 @@ db.define_table('quizzes',
     Field('tags', 'list:reference db.tags'),
     Field('frequency'),
     Field('groups', 'list:reference db.auth_group'),
-    Field('start_date', 'date', default=dtnow),
+    Field('start_date', 'date', default = dtnow),
     Field('end_date', 'date'),
-    format='%(quiz)s')
+    format = '%(quiz)s')
 
 db.quizzes.tags.requires = IS_IN_DB(db, 'tags.id', db.tags._format, multiple = True)
 db.quizzes.groups.requires = IS_IN_DB(db, 'auth_group.id', db.auth_group._format, multiple = True)
 
 db.define_table('question_records',
-    Field('name', db.auth_user, default=auth.user_id),
+    Field('name', db.auth_user, default = auth.user_id),
     Field('question', db.questions),
     Field('times_right', 'double'),
     Field('times_wrong', 'double'),
-    Field('tlast_wrong', 'datetime', default=dtnow),
-    Field('tlast_right', 'datetime', default=dtnow),
+    Field('tlast_wrong', 'datetime', default = dtnow),
+    Field('tlast_right', 'datetime', default = dtnow),
     Field('category', db.categories)
     )
 
 db.define_table('tag_records',
-    Field('name', db.auth_user, default=auth.user_id),
+    Field('name', db.auth_user, default = auth.user_id),
     Field('tag', db.tags),
     Field('times_right', 'double'),
     Field('times_wrong', 'double'),
-    Field('tlast_wrong', 'datetime', default=dtnow),
-    Field('tlast_right', 'datetime', default=dtnow),
+    Field('tlast_wrong', 'datetime', default = dtnow),
+    Field('tlast_right', 'datetime', default = dtnow),
     Field('category', db.categories)
     )
 
 db.define_table('attempt_log',
-    Field('name', db.auth_user, default=auth.user_id),
+    Field('name', db.auth_user, default = auth.user_id),
     Field('question', db.questions),
     Field('score', 'double'),
     Field('quiz', db.quizzes),
-    Field('dt_attempted', 'datetime', default=dtnow)
+    Field('dt_attempted', 'datetime', default = dtnow)
     )
 
 db.define_table('quiz_records',
-    Field('name', db.auth_user, default=auth.user_id),
+    Field('name', db.auth_user, default = auth.user_id),
     Field('quiz', db.quizzes),
     Field('score', 'double'),
-    Field('date_taken', 'datetime', default=dtnow)
+    Field('date_taken', 'datetime', default = dtnow)
     )
 
 db.define_table('bug_status',
     Field('status_label'),
-    format='%(status_label)s')
+    format = '%(status_label)s')
 
 db.define_table('q_bugs',
     Field('question', db.questions),
     Field('a_submitted'),
-    Field('name', db.auth_user, default=auth.user_id),
-    Field('submitted', 'datetime', default=dtnow),
-    Field('bug_status', db.bug_status, default=1),
-    Field('admin_comment',  'text'),
-    Field('prev_lastright',  'datetime'),
+    Field('name', db.auth_user, default = auth.user_id),
+    Field('submitted', 'datetime', default = dtnow),
+    Field('bug_status', db.bug_status, default = 1),
+    Field('admin_comment', 'text'),
+    Field('prev_lastright', 'datetime'),
     Field('prev_lastwrong', 'datetime'),
-    format='%(question)s')
+    format = '%(question)s')
 
 db.define_table('news',
     Field('story', 'text'),
     Field('title', 'string'),
-    Field('name', db.auth_user, default=auth.user_id),
-    Field('date_submitted', 'datetime', default=dtnow),
-    Field('last_edit', 'datetime', default=dtnow),
-    format='%(title)s')
+    Field('name', db.auth_user, default = auth.user_id),
+    Field('date_submitted', 'datetime', default = dtnow),
+    Field('last_edit', 'datetime', default = dtnow),
+    format = '%(title)s')
--- modules/plugin_ajaxselect.py
+++ modules/plugin_ajaxselect.py
@@ -1,22 +1,23 @@
-from gluon import sqlhtml, current, SPAN, A, DIV
+from gluon import current, SPAN, A, DIV #sqlhtml
 from gluon.custom_import import track_changes
 from gluon.html import URL
 from gluon.sqlhtml import OptionsWidget, ListWidget, MultipleOptionsWidget
-from plugin_multiselect_widget import vmultiselect_widget, hmultiselect_widget
 
 session, request, response = current.session, current.request, current.response
-response.files.append(URL('static','plugin_ajaxselect/plugin_ajaxselect.js'))
+response.files.append(URL('static', 'plugin_ajaxselect/plugin_ajaxselect.js'))
 
 #TODO: set track changes to false when dev is finished
 track_changes(True)
 
 class AjaxSelect:
     """
-    This plugin creates a select widget wrapped that can be refreshed via ajax without resetting
-    the entire form. It also provides an "add new" button that allows users to add a new item to
-    the table that populates the select widget via ajax. The widget is then automatically refreshed
-    via ajax so that the new item is visible as a select option and can be chosen. All of this
-    happens without a page or form refresh so that data entered in other fields is not lost or submitted.
+    This plugin creates a select widget wrapped that can be refreshed via ajax 
+    without resetting the entire form. It also provides an "add new" button 
+    that allows users to add a new item to the table that populates the select 
+    widget via ajax. The widget is then automatically refreshed via ajax so that 
+    the new item is visible as a select option and can be chosen. All of this
+    happens without a page or form refresh so that data entered in other fields 
+    is not lost or submitted.
 
     Installation:
     1. download the plugin file;
@@ -33,7 +34,7 @@ class AjaxSelect:
     'author' from a table named 'notes' you would add this line somewhere in
     the model file:
 
-    db.notes.author.widget = lambda field, value: AjaxSelect(field, value, 'authors', {optional argument}).widget()
+    db.notes.author.widget = lambda field, value: AjaxSelect(field, value, 'authors', {optional arguments}).widget()
 
     Note that the third argument passed to AjaxSelect should be the name of the
     table *referenced by the current field*. In this example, the field 'author'
@@ -44,21 +45,27 @@ class AjaxSelect:
     widget via ajax.
 
     adder (True/False; defaults to True): a button to add a new record to the linked
-        table that populates the select widget.
+    table that populates the select widget.
 
-    restrictor ({form field name}): adds a dynamic constraint on the records displayed in the
-    select widget. When the specified form field (within the same form) has its value
-    changed, this select will be refreshed and its displayed records filtered
-    accordingly. Note that this is only useful if {fieldname} references values shared
-    with the linked table.
+    restrictor ({form field name}): adds a dynamic constraint on the records displayed 
+    in the named field's widget. When the specified form field (within the same form) 
+    has its value changed, this select will be refreshed and its displayed records 
+    filtered accordingly. Note that this is only useful if {fieldname} references 
+    values shared with the linked table.
 
     e.g., to make the select constrain the widget for the 'works' table:
     db.notes.author.widget = lambda field, value: AjaxSelect(field, value, 'authors', restrictor='work').widget()
 
+    multi (basic/False; defaults to False): Instead of displaying a single select 
+    widget, the 'basic' value displays a standard multiselect widget (an html 
+    select widget with a size greater than 1). This will only work properly if the 
+    database field type is defined as "list:reference" in the model. 
+
     """
     #TODO: allow for restrictor argument to take list and filter multiple other fields
 
-    def __init__(self, field, value, linktable, refresher = False, adder = True, restricted = "None", restrictor = "None", multi = False):
+    def __init__(self, field, value, linktable, refresher = False, adder = True,
+                 restricted = "None", restrictor = "None", multi = False):
         self.field = field
         self.value = value
         self.linktable = linktable
@@ -89,7 +96,7 @@ class AjaxSelect:
 
     def build_info(self):
         #build name for the span that will wrap the select widget
-        self.wrappername = '%s_%s_loader' %(self.tablename, self.fieldname)
+        self.wrappername = '%s_%s_loader' % (self.tablename, self.fieldname)
 
         #classes for wrapper span to indicate filtering relationships
         if self.restrictor == 'None':
@@ -98,34 +105,37 @@ class AjaxSelect:
             self.classes += '%s restrictor for_%s' % (self.linktable, self.restrictor)
 
         #URL to reload widget via ajax
-        self.comp_url = URL('plugin_ajaxselect', 'set_widget.load', args=[self.tablename, self.fieldname, self.clean_val, self.linktable, self.wrappername], vars=dict(multi=self.multi))
+        self.comp_url = URL('plugin_ajaxselect', 'set_widget.load',
+            args = [self.tablename, self.fieldname, self.clean_val, self.linktable, self.wrappername],
+            vars = dict(multi = self.multi))
 
         #URL to load form for linking table via ajax
-        self.add_url = URL('plugin_ajaxselect', 'set_form_wrapper.load', args=[self.tablename, self.fieldname, self.clean_val, self.linktable, self.wrappername])
+        self.add_url = URL('plugin_ajaxselect', 'set_form_wrapper.load',
+            args = [self.tablename, self.fieldname, self.clean_val, self.linktable, self.wrappername])
 
     def create_widget(self):
         #create the select widget
         self.adder_id = '%s_add_trigger' % self.linktable
         self.refresher_id = '%s_refresh_trigger' % self.linktable
-        if self.multi == 'v':
+        if self.multi == 'basic':
             self.w = MultipleOptionsWidget.widget(self.field, self.value)
-        elif self.multi == 'h':
-            self.w = ListWidget.widget(self.field, self.value)
         else:
             self.w = OptionsWidget.widget(self.field, self.value)
 
     def create_wrapper(self):
-        #create the span wrapper and place the select widget inside, along with buttons to add and refresh if necessary
-        span = SPAN(self.w, _id=self.wrappername, _class = self.classes)
+        #create the span wrapper and place the select widget inside, 
+        #along with buttons to add and refresh if necessary
+        span = SPAN(self.w, _id = self.wrappername, _class = self.classes)
+        #hide refresher via CSS if necessary
         if self.refresher is False:
             rstyle = 'display:none'
         else:
             rstyle = ''
-        refresh_a = A('refresh', _href=self.comp_url, _id=self.refresher_id, cid=self.wrappername, _style=rstyle)
-        form_name= '%s_adder_form' % self.linktable
-        adder = A('add new', _href=self.add_url, _id=self.adder_id, _class='add_trigger', cid=form_name)
+        refresh_a = A('refresh', _href = self.comp_url, _id = self.refresher_id, cid = self.wrappername, _style = rstyle)
+        form_name = '%s_adder_form' % self.linktable
+        adder = A('add new', _href = self.add_url, _id = self.adder_id, _class = 'add_trigger', cid = form_name)
         #TODO: Is this dialog element still necessary?
-        dialog = DIV('', _id=form_name)
+        dialog = DIV('', _id = form_name)
 
         if not self.adder:
             self.wrapper = span, refresh_a, dialog
