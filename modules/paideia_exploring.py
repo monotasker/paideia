ac7ce28c5f12e53a34d6b4d7e25d11807518df70
Ian W. Scott <iscott@tyndale.ca>
2012-02-24 22:30:57 -0500

    More work on check() method of paideia_path class

--- modules/paideia_exploring.py
+++ modules/paideia_exploring.py
@@ -9,20 +9,44 @@ class paideia_path:
         session, request, auth, db = current.session, current.request, current.auth, current.db
 
     def check(self):
-        """Find out whether to introduce another step, free the user for movement, or continue with 
+        """Find out whether to introduce another step, free the user for movement, or continue with
         the current step."""
-        
+
         #current object must be accessed at runtime, so can't be global variable
         session, request, auth, db = current.session, current.request, current.auth, current.db
-        
+
         #has the user completed enough paths for today?
         if session.completed_paths:
-            len(session.completed_paths)
-        
-        
+            todays = len(session.completed_paths)
+            rsetting = db(db.app_settings.id == 1).select().first()
+            required = rsetting.paths_per_day
+            if todays >= required:
+                return('done')
+                pass
+
+        #is there any blocking state in effect?
+        if session.block:
+            return('blocked')
+            pass
+
+        #check to see whether there are any paths active for this location
+        if session.active_paths:
+            #find list of active paths
+            active = session.active_paths
+            #TODO: find out last completed step in path
+
+            #find current location
+            loc = request.vars[loc]
+
+            for p in active:
+                #find the next incomplete step in the active path
+                steps = db((db.paths.steps == db.steps.id) & (db.paths.id == p)).select()
+                #check to see whether the step can be completed in this location
+
+
     def pick(self):
-        """Choose a new path for the user, based on tag performance"""    
-        
+        """Choose a new path for the user, based on tag performance"""
+
     def set(self):
         #current object must be accessed at runtime, so can't be global variable
         session, request, auth, db = current.session, current.request, current.auth, current.db
@@ -34,11 +58,11 @@ class paideia_path:
             session.path_name = the_path[0].quiz
             session.path_freq = the_path[0].frequency
             session.path_tags = the_path[0].tags
-    
+
     def end(self):
         #current object must be accessed at runtime, so can't be global variable
-        session, request, auth, db = current.session, current.request, current.auth, current.db        
-        
+        session, request, auth, db = current.session, current.request, current.auth, current.db
+
         pass
 
 class counter:
@@ -46,7 +70,7 @@ class counter:
     def __init__(self):
         """include this question in the count for this quiz, send to 'end' if quiz is finished"""
 
-    def check(self):        
+    def check(self):
         #current object must be accessed at runtime, so can't be global variable
         session, request = current.session, current.request
 
@@ -62,17 +86,17 @@ class counter:
 
     def set(self):
         pass
-        
+
     def clear(self):
         pass
-        
+
 class map:
-    
+
     def __init__(self):
-                
+
         #current object must be accessed at runtime, so can't be global variable
         db = current.db
-        
+
         #prepare map interface for user to select a place to go
         self.locs = db().select(db.locations.ALL, orderby=db.locations.location)
         self.image = '/paideia/static/images/town_map.svg'
