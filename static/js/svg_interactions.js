a2b9e828f44fe44e29888ba01736e86c0e207bca
Ian W. Scott <iscott@tyndale.ca>
2012-02-28 15:17:27 -0500

    svg town_map now working for basic navigation (just domusA so far)

--- static/js/svg_interactions.js
+++ static/js/svg_interactions.js
@@ -9,18 +9,3 @@ function go_there(evt){
     var oname = tobj.getAttribute('id');
     window.parent.web2py_component("/paideia/exploring/index.load?loc=" + oname,"page")
 }
-
-function pulse_in(evt){
-    var tobj = evt.currentTarget;   
-    
-    tobj.setAttribute('stroke-opacity', '1');
-    //bbox =  tobj.getBBox();
-    //dX = bbox.width * (-.05);
-    //dY = bbox.height * (-.1);
-    //tobj.setAttribute('transform', 'scale(1.1), translate(' + dX + ', ' + dY + ')');
-}
-
-function pulse_out(evt){
-    var tobj = evt.currentTarget;
-    tobj.setAttribute('stroke-opacity', '0');
-}
