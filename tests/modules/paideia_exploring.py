a6aac37aa2b8a46bbce13f386e1017f87ad922b7
Ian W. Scott <iscott@tyndale.ca>
2012-04-16 20:37:17 -0400

    Significant refactoring

--- tests/modules/paideia_exploring.py
+++ tests/modules/paideia_exploring.py
@@ -3,14 +3,30 @@
 #found when running python2.7 web2py.py -S paideia -M -R testRunner.py
 
 import unittest
-from gluon.globals import Request, Session, Storage, Response
+import cPickle as pickle
+from gluon import * 
 from gluon.contrib.test_helpers import form_postvars
+from gluon import current
 
-class Paideia_exploringModule(unittest.TestCase):
-	def setUp(self):
-		pass
+class Paideia_exploringModules(unittest.TestCase):
+    """unit test suite for paideia module"""
 
-	def testIndex(self):
-		#form_postvars()
-		self.assertEquals(2,3)
-		self.assertEquals(2,2)
+    def __init__(self, p):
+        global auth, session, request
+        unittest.TestCase.__init__(self, p)
+        self.session = pickle.dumps(current.session)
+        current.request.application = 'welcome'
+        current.request.controller = 'default'
+        self.request = pickle.dumps(current.request)
+
+    def setUp(self):
+        global response, session, request, auth
+        current.session = pickle.loads(self.session)
+        current.request = pickle.loads(self.request)
+        auth = Auth(globals(), db)
+        auth.define_tables()
+
+    def testTag(self):
+        #form_postvars()
+        self.assertEquals(2,3)
+        self.assertEquals(2,2)
