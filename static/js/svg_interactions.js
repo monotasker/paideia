596a04b27d1cedb20a5c11590c65102c672eb9d1
Ian W. Scott <iscott@tyndale.ca>
2012-05-07 15:01:58 -0400

    Various minor changes to town map and related files; also restored plugin_listandedit controller which was accidentally deleted in an earlier commit (git status)

--- static/js/svg_interactions.js
+++ static/js/svg_interactions.js
@@ -9,3 +9,12 @@ function go_there(evt){
     var oname = tobj.getAttribute('id');
     window.parent.web2py_component("/paideia/exploring/walk.load/ask?loc=" + oname,"page");
 }
+
+function mask_other(evt){
+    var tobj = evt.currentTarget;
+    var tname = tobj.getAttribute('id');
+    var maskname = tname + '_mask';
+    document.getElementById(maskname).style.display = 'block';
+    document.getElementById(maskname).style.opacity = '1';
+
+}
