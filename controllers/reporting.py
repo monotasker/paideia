911c33e8dadf93530afd09fd92eead2a6691946c
Ian W. Scott <iscott@tyndale.ca>
2012-08-14 12:15:07 -0400

    Updated controller default.user() to use new Stats and Bug classes.

--- controllers/reporting.py
+++ controllers/reporting.py
@@ -15,7 +15,6 @@ def index():
     reports = dict(attempts='Attemps Log',)
     return dict(reports=reports)
 
-
 @auth.requires_membership(role='administrators')
 def paths_by_tag():
     tags = db(db.tags.id > 0).select()
@@ -37,30 +36,30 @@ def paths_by_tag():
 
     return dict(taglist=taglist)
 
-
 @auth.requires_membership(role='administrators')
 def attempts():
-    form = SQLFORM.grid(db.attempt_log)
+    if len(request.args) > 0:
+        form = SQLFORM.grid(db.attempt_log.name == request.args[0])
+    else:
+        form = SQLFORM.grid(db.attempt_log)
     return dict(form=form)
-
 
 @auth.requires_membership(role='administrators')
 def user():
     # get student's user id and name
     the_user = db.auth_user[request.args[0]]
     the_name = the_user.last_name + ', ' + the_user.first_name
-    # create instance of paideia_stats class to calculate student performance
-    s = paideia_stats(request.args[0])
-    t = paideia_timestats(request.args[0])
-    w = paideia_weeklycount(request.args[0])
-    # create dynamic grid to display list of all question attempts
-    #form = SQLFORM.grid(db.attempt_log.name == request.args[0])
-    #place form=auth() in returned values
 
-    return dict(the_name=the_name, score_avg=s.score_avg,
-        total_len=t.total_len, total_cat1=t.total_cat1,
-        total_cat2=t.total_cat2, total_cat3=t.total_cat3,
-        percent_cat1=t.percent_cat1, percent_cat2=t.percent_cat2,
-        percent_cat3=t.percent_cat3, total_cat4=t.total_cat4,
-        percent_cat4=t.percent_cat4, dateset=w.dateset,
-        htmlcal=w.htmlcal)
+    s = Stats(request.args[0])
+    avg = s.average()
+    cats = s.categories()
+    cal = s.monthcal()
+
+    b = Bug()
+    blist = b.bugresponses(the_user)
+
+    return dict(the_name=the_name,
+            score_avg=avg,
+            categories=cats,
+            calendar=cal
+            blist=blist)
