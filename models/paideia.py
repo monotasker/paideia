a1d20c060df6eb5c855d25f4ab95aaae00869c60
Ian W. Scott <iscott@tyndale.ca>
2012-10-07 21:43:04 -0400

    Finished separating max-badge achievement from review status in back-end

--- models/paideia.py
+++ models/paideia.py
@@ -289,6 +289,7 @@ db.define_table('tag_progress',
     Field('rev1', 'list:reference tags'),
     Field('rev2', 'list:reference tags'),
     Field('rev3', 'list:reference tags'),
+    Field('rev4', 'list:reference tags'),
     format='%(name)s, %(latest_new)s')
 db.tag_progress.name.requires = IS_NOT_IN_DB(db, db.tag_progress.name)
 
