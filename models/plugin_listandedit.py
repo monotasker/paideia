38be7590e9d1594816ab2f3bd0c2014ec60c3c8f
Ian W. Scott <iscott@tyndale.ca>
2012-11-15 05:47:14 -0500

    Moved all plugins to separate folder as git submodules.

--- models/plugin_listandedit.py
+++ models/plugin_listandedit.py
@@ -1,18 +1,1 @@
-if 0:
-    from gluon import current, URL
-    response = current.response
-
-
-response.files.append(URL('static',
-            'plugin_listandedit/plugin_listandedit.css'))
-response.files.append(URL('static',
-            'plugin_listandedit/plugin_listandedit.js'))
-
-
-def plugin_listandedit():
-    '''
-    Public interface method to call in views in order to embed the
-    plugin_listandedit widget.
-    '''
-    return LOAD('plugin_listandedit', 'widget.load', ajax=True)
-
+../plugins/plugin_listandedit/models/plugin_listandedit.py
