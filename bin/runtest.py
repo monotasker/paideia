52928cd39d52f9a996cca8d7f792bf27b745603b
Ian Scott <scottianw@gmail.com>
2012-12-05 14:06:24 -0500

    working pytest test runner file

--- bin/runtest.py
+++ bin/runtest.py
@@ -1,23 +1,23 @@
 '''
 Run this app's tests via py.test
 
-run with: python web2py.py -S paideia -R applications/paideia/bin/runtest.py
+run with: python ~/web/web2py/web2py.py -S paideia -R applications/paideia/bin/runtest.py
 '''
 import subprocess
 import os
 import sys
 from copy import copy
 
-def run_execfile(test_dir, app_name):
+def run_execfile(w2p_dir, test_dir, app_name):
     if os.name == 'nt':
         errlist = (WindowsError,ValueError,SystemExit)
     else:
         errlist = (OSError,ValueError,SystemExit)
 
     try:
-        file_dir = test_dir # os.path.split(test_file)[0]
-        if file_dir not in sys.path:
-            sys.path.append(file_dir)   # to support imports from current folder in the testfiles
+        test_dir = os.path.join(w2p_dir, 'applications', app_name, test_dir)
+        if test_dir not in sys.path:
+            sys.path.append(test_dir)   # to support imports from current folder in the testfiles
         # modules are applications/[app_name]/modules
         modules_path = os.path.join('applications', app_name, 'modules')
         if modules_path not in sys.path:
@@ -25,15 +25,20 @@ def run_execfile(test_dir, app_name):
         if 'site-packages' not in sys.path:
             sys.path.append('site-packages')    # support imports from web2py/site-packages
         g = copy(globals())
-        subprocess.Popen("py.test {}".format(test_dir), stdout=PIPE, shell=True).stdout.read()
-        #execfile(test_file, g,)
+        proc = subprocess.Popen("py.test {}".format(test_dir),
+                                    stdout=subprocess.PIPE,
+                                    stderr=subprocess.PIPE,
+                                    shell=True)
+        stdout_val, stderr_val = proc.communicate()
+        print stdout_val, stderr_val
+
     except errlist:
-        pass # we know about the rotating logger error...
-             # and SystemExit is not useful to detect
+        pass
+
     except Exception, e:
         #showfeedback()
         print type(e), e
 
 if __name__=='__main__':
-    run_execfile('../tests/', 'paideia')
+    run_execfile('~/web/web2py/', 'tests', 'paideia')
 
