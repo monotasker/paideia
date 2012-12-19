d5f53b556899df94ffa6243ad93fccb4e56a1e45
Ian W. Scott <iscott@tyndale.ca>
2012-12-18 22:11:09 -0500

    Extensive work on refactor and tests for it using pytest

--- tests/modules/test_paideia.py
+++ tests/modules/test_paideia.py
@@ -1,25 +1,10 @@
 # Unit tests for the paideia module
 
-import sys
 import pytest
-
-sys.path.append('/home/ian/web/web2py/applications/paideia/modules')
-sys.path.append('/home/ian/web/web2py')
-
-from paideia import Npc
+from paideia import Npc, Location, Step, User
 from gluon import *
-from gluon.storage import Storage
-from gluon.shell import exec_environment
 
-#db = exec_environment('applications/paideia/models/paideia.py')
-
-#db = DAL('sqlite://storage.sqlite')
-
-db = Storage({'npcs': Storage({
-            1: Storage({'id': 1, 'name': 'Alexander'}),
-            2: Storage({'id': 2, 'name': 'Maria'}),
-        })
-})
+db = current.db
 
 @pytest.fixture
 def mynpc():
@@ -28,13 +13,27 @@ def mynpc():
     '''
     return Npc(1, db)
 
+@pytest.fixture
+def myloc():
+    """
+    A pytest fixture providing a paideia.Location object for testing.
+    """
+    return Location(6, db)
+
+@pytest.fixture
+def mystep():
+    """
+    A pytest fixture providing a paideia.Step object for testing.
+    """
+    return Step(1, db)
+
 class TestNpc():
     '''
     Tests covering the Npc class of the paideia module.
     '''
     def test_npc_get_name(self, mynpc):
         """Test for method Npc.get_name()"""
-        assert mynpc.get_name() == 'Alexander'
+        assert mynpc.get_name() == 'Ἀλεξανδρος'
 
     def test_npc_get_id(self, mynpc):
         """Test for method Npc.get_id()"""
@@ -42,24 +41,75 @@ class TestNpc():
 
     def test_npc_get_image(self, mynpc):
         """Test for method Npc.get_image()"""
-        pass
+        assert mynpc.get_image().xml() == '<img src="/paideia/static/images/images.image.bb48641f0122d2b6.696d616765732e696d6167652e383136303330663934646664646561312e34343732363137373639366536373230333432653733373636372e737667.svg" />'
 
-    def test_npc_get_locations(self, mynpc)
+    def test_npc_get_locations(self, mynpc):
         """Test for method Npc.get_locations()"""
-        pass
+        locs = mynpc.get_locations()
+        assert isinstance(locs[0], Location)
+        assert locs[0].get_id() == 6
+        assert isinstance(locs[1], Location)
+        assert locs[1].get_id() == 8
+
+    def test_npc_get_description(self, mynpc):
+        """Test for method Npc.get_description()"""
+        assert mynpc.get_description() == "Ἀλεξανδρος is a shop owner and good friend of Simon's household. His shop is in the agora."
+
+    def test_npc_choose(self, mynpc):
+        """Test for method Npc.choose()"""
+        pass # TODO: write this test
+
+class TestLocation():
+    """
+    Tests covering the Location class of the paideia module.
+    """
+    # TODO: parameterize to cover more locations
+
+    def test_location_get_alias(self, myloc):
+        """Test for method Location.get_alias"""
+        assert myloc.get_alias() == 'shop_of_alexander'
+
+    def test_location_get_name(self, myloc):
+        """Test for method Location.get_name"""
+        assert myloc.get_name() == 'πανδοκεῖον Ἀλεξανδρου'
+
+    def test_location_get_readable(self, myloc):
+        """Test for method Location.get_readable"""
+        assert myloc.get_readable() == 'ἡ ἀγορά'
+
+    def test_location_get_bg(self, myloc):
+        """Test for method Location.get_bg"""
+        assert myloc.get_bg().xml() == '<img src="/paideia/static/images/images.image.9a515ff664f03aa3.323031322d30372d32312032335f35315f31322e706e67.png" />'
+    def test_location_get_id(self, myloc):
+        """Test for method Location.get_id"""
+        assert myloc.get_id() == 6
+
+class TestStep():
+    """
+    Unit tests covering the Step class of the paideia module.
+    """
+    # TODO: parameterize to cover more locations
+
+    def test_step_get_id(self, mystep):
+        """Test for method Step.get_id"""
+        assert mystep.get_id() == 1
+
+    def test_step_get_prompt(self, mystep):
+        """Test for method Step.get_prompt"""
+        assert mystep.get_prompt() == 'How could you write the word "meet" using Greek letters?'
+
+    def test_step_get_locations(self, mystep):
+        """Test for method Step.get_locations"""
+        assert mystep.get_locations() == [3, 1, 13, 7, 8, 11]
+
+    def test_location_get_id(self, mystep):
+        """Test for method Step.get_id"""
+        assert mystep.get_id() == 1
+
+
+class TestUser():
+    pass
 
 class TestStance():
     pass
 
-#class StanceTest(unittest.TestCase):
-    #def setUp(self):
-        #self.stance = Stance()
-
-    #def get_start_time_test(self):
-        #time = self.stance.get_start_time()
-
-    #def tearDown(self):
-        #pass
-
-#if __name__ == "main":
-    #unittest.main()
