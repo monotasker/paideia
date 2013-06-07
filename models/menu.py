649dd37b99845e72e0589365be3579dbc5f6cd9a
Ian W. Scott <iscott@tyndale.ca>
2013-06-07 19:09:46 -0400

    Significant submodule changes as part of migrating framework to bootstrap

--- models/menu.py
+++ models/menu.py
@@ -1,79 +1,105 @@
 # -*- coding: utf-8 -*-
 
 if 0:
-    from gluon import current, URL, SPAN
+    from gluon import current, URL, SPAN, XML
     response, request, T = current.response, current.request, current.t
     auth = current.auth
+from datetime import datetime
+
+"""
+This file includes the menu content along with other meta content and global
+layout settings
+"""
+
+# Meta =====================================================================
 
 response.title = request.application
 response.mobiletitle = request.application
-response.subtitle = T('An online space for learning New Testament Greek')
+response.subtitle = T('Learning New Testament Greek in Context')
 
 #http://dev.w3.org/html5/markup/meta.name.html
 response.meta.author = 'Ian W. Scott'
-response.meta.description = 'An online space for learning New Testament Greek'
-response.meta.keywords = \
-                'Greek, koine, New Testament, language, education, learning'
+response.meta.description = 'An online, interactive course in ' \
+                            'New Testament Greek'
+response.meta.keywords = 'Greek, koine, New Testament, language, ' \
+                         'education, learning'
 response.meta.generator = 'Web2py Enterprise Framework'
-response.meta.copyright = 'Copyright 2011-2012, Ian W. Scott. Source' \
-    'code available on <a href="https://github.com/monotasker/paideia">' \
-    'GitHub</a>'
+response.meta.copyright = XML('All content copyright &copy; 2011-{}, Ian W. Scott. Source '
+    'code available on <a href="https://github.com/monotasker/paideia">GitHub</a>'.format(datetime.now().year))
 
+# Layout ===================================================================
 
-response.menu = [
-    (T('Home'), False, URL('default', 'index'), []),
-    (SPAN(T('Map'), _class='icon-location'), False,
-                                             URL('exploring', 'index'), []),
-    (SPAN(T('Slides'), _class='icon-image'), False,
-                                             URL('listing', 'slides'), [])
-    ]
+response.left_sidebar_enabled = 1
+response.right_sidebar_enabled = 1
+
+# Menu =====================================================================
+
+response.menu = [(SPAN(T(' Home'), _class='icon-home'), False,
+                  URL('default', 'index'), []),
+                 (SPAN(T(' Map'), _class='icon-map-marker'), False,
+                  URL('exploring', 'index'), []),
+                 (SPAN(T(' Slides'), _class='icon-picture'), False,
+                  URL('listing', 'slides'), [])
+                 ]
+m = response.menu
 
 if auth.has_membership('administrators', auth.user_id):
-    response.menu += [(SPAN(T('Admin'), _class='icon-only icon-gear'), False, None, [
-            (T('Create'), False, None, [
-                (T('Slide decks'), False, URL('editing',
+    m += [(SPAN(T(' Admin'), _class='icon-cog'), False, None,
+           [(SPAN(T(' Create'), _class='icon-leaf'), False, None,
+             [(SPAN(T(' Slide decks'), _class='icon-tasks'), False, URL('editing',
                                             'listing.html',
                                             args=['plugin_slider_decks'])),
-                (T('Slides'), False, URL('editing',
-                                        'listing.html',
-                                        args=['plugin_slider_slides'],
-                                        vars={'orderby':'slide_name'})),
-                (T('Paths'), False, URL('editing',
-                            'listing.html', args=['paths'])),
-                (T('Steps'), False, URL('editing',
-                            'listing.html', args=['steps'])),
-                (T('Tags'), False, URL('editing',
-                            'listing.html', args=['tags'])),
-                (T('Badges'), False, URL('editing',
-                            'listing.html', args=['badges'])),
-                (T('Instructions'), False, URL('editing',
-                            'listing.html', args=['step_instructions'])),
-                (T('Hints'), False, URL('editing',
-                            'listing.html', args=['step_hints'])),
-                (T('NPCs'), False, URL('editing',
-                            'listing.html', args=['npcs'])),
-                (T('locations'), False, URL('editing',
-                            'listing.html', args=['locations'])),
-                (T('images'), False, URL('editing',
-                            'listing.html', args=['images'])),
-            ]),
+              (SPAN(T(' Slides'), _class='icon-film'), False, URL('editing',
+                                       'listing.html',
+                                       args=['plugin_slider_slides'],
+                                       vars={'orderby': 'slide_name'})),
+              (SPAN(T(' Paths'), _class='icon-road'), False, URL('editing',
+                                      'listing.html',
+                                      args=['paths'])),
+              (SPAN(T(' Steps'), _class='icon-plus-sign-alt'), False, URL('editing',
+                                      'listing.html',
+                                      args=['steps'])),
+              (SPAN(T(' Tags'), _class='icon-tag'), False, URL('editing',
+                                     'listing.html',
+                                     args=['tags'])),
+              (SPAN(T(' Badges'), _class='icon-certificate'), False, URL('editing',
+                                       'listing.html',
+                                       args=['badges'])),
+              (SPAN(T(' Instructions'), _class='icon-check'), False, URL('editing',
+                                             'listing.html',
+                                             args=['step_instructions'])),
+              (SPAN(T(' Hints'), _class='icon-question'), False, URL('editing',
+                                      'listing.html',
+                                      args=['step_hints'])),
+              (SPAN(T(' NPCs'), _class='icon-group'), False, URL('editing',
+                                     'listing.html',
+                                     args=['npcs'])),
+              (SPAN(T(' locations'), _class='icon-screenshot'), False, URL('editing',
+                                          'listing.html',
+                                          args=['locations'])),
+              (SPAN(T(' images'), _class='icon-picture'), False, URL('editing',
+                                       'listing.html',
+                                       args=['images'])),
+              ]),
 
-            (T('Reports'), False, None, [
-                (T('Users'), False, URL('listing', 'user')),
-                (T('New Bug reports'), False, URL('editing',
+            (SPAN(T(' Reports'), _class='icon-bar-chart'), False, None,
+             [(SPAN(T(' Users'), _class='icon-group'), False, URL('listing', 'user')),
+              (SPAN(T(' New Bug reports'), _class='icon-warning-sign'), False, URL('editing',
                                         'listing.html', args=['bugs'],
-                                        vars={'restrictor':{'bug_status':5}})),
-                (T('Confirmed Bug reports'), False, URL('editing',
+                                        vars={'restrictor': {'bug_status': 5}})),
+              (SPAN(T(' Confirmed Bug reports'), _class='icon-stethoscope'), False, URL('editing',
                                         'listing.html', args=['bugs'],
-                                        vars={'restrictor':{'bug_status':1}})),
-                (T('All Bug reports'), False, URL('editing',
+                                        vars={'restrictor': {'bug_status': 1}})),
+              (SPAN(T(' All Bug reports'), _class='icon-frown'), False, URL('editing',
                                         'listing.html', args=['bugs'])),
-                (T('Paths by tag'), False, URL('reporting',
+              (SPAN(T(' Paths by tag'), _class='icon-tags'), False, URL('reporting',
                                         'paths_by_tag')),
-                (T('Attempt log'), False, URL('reporting',
+              (SPAN(T(' Attempt log'), _class='icon-list-alt'), False, URL('reporting',
                                         'attempts')),
-            ]),
-            (T('Web IDE'), False, URL('admin', 'default', 'index')),
-            (T('Database'), False, URL('appadmin', 'index')),
-        ])
-        ]
+              ]),
+
+            (SPAN(T(' Web IDE'), _class='icon-code'), False, URL('admin', 'default', 'index')),
+
+            (SPAN(T(' Database'), _class='icon-sitemap'), False, URL('appadmin', 'index')),
+            ])
+          ]
