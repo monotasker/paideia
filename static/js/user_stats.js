334b465a41c8252d0d20a4248ed0296d094d14d7
Matej Grabovsky <mgrabovsky@yahoo.com>
2014-07-14 22:37:58 +0200

    Finished answers chart (vis2)
    Note: there's a problem with visibility for long time ranges

--- static/js/user_stats.js
+++ static/js/user_stats.js
@@ -25,21 +25,22 @@
         badge_set_milestones = user_stats.badge_set_milestones
             .map(function(d) {
                 d.date = parse_date(d.date);
-                d.badge_set |= 0;
                 return d;
             });
 
         answer_counts = user_stats.answer_counts
             .map(function(d) {
-                d.date = parse_date(d.date);
-                d.right |= 0;
-                d.wrong |= 0;
+                d.date  = parse_date(d.date);
+                d.total = d.right + d.wrong
+                d.ys    = [{ 'class': 'right', y0: 0, y1: d.right },
+                           { 'class': 'wrong', y0: d.right, y1: d.right + d.wrong }];
+                return d;
             });
     };
 
-    // @TODO: Factor out a Chart class
+    // TODO: Factor out a Chart class
     var render_milestones = function(opts) {
-        // @TODO: Defaults `opts`
+        // TODO: Defaults `opts`
         // Conventional margins
         width  = opts.width - opts.margin.left - opts.margin.right
                     - opts.inner_margin.left - opts.inner_margin.right;
@@ -110,6 +111,82 @@
            .call(y_axis);
     };
 
+    var render_answers = function(opts) {
+        // Conventional margins
+        width  = opts.width - opts.margin.left - opts.margin.right
+                    - opts.inner_margin.left - opts.inner_margin.right;
+        height = opts.height - opts.margin.top - opts.margin.bottom
+                    - opts.inner_margin.top - opts.inner_margin.bottom;
+
+        // Set up axes
+        var time = d3.time.scale()
+                     .domain(d3.extent(answer_counts, function(d) { return d.date; }))
+                     .range([0, width]);
+        var x = d3.scale.ordinal()
+                  .domain(d3.time.days(time.domain()[0],
+                                       d3.time.day.offset(time.domain()[1], 1)))
+                  .rangeBands([0, width], 0, 0);
+        var y = d3.scale.linear()
+                  .domain([0, d3.max(answer_counts, function(d) { return d.total; })])
+                  .rangeRound([height, 0]);
+        var x_axis = d3.svg.axis()
+                       .scale(time)
+                       .orient('bottom')
+                       .tickFormat(format_date)
+                       .outerTickSize(0);
+        var y_axis = d3.svg.axis()
+                       .scale(y)
+                       .orient('left')
+                       .tickFormat(d3.format('d'))
+                       .outerTickSize(0);
+
+        // Construct SVG container
+        var svg = d3.select(opts.container)
+                    .append('svg')
+                        .attr('class', 'chart')
+                        .attr('width', opts.width)
+                        .attr('height', opts.height)
+                    .append('g')
+                        .attr('transform', 'translate(' + opts.margin.left + ','
+                                + opts.margin.top + ')');
+
+        // Plot the data
+        var inner = svg.append('g')
+                        .attr('class', 'inner')
+                        .attr('transform', 'translate(' + opts.inner_margin.left + ','
+                                    + opts.inner_margin.top + ')');
+
+        var group = inner.selectAll('.g')
+                         .data(answer_counts)
+                     .enter().append('g')
+                        .attr('class', 'g')
+                        .attr('transform', function(d) { return 'translate(' + x(d.date) + ',0)'; });
+        group.selectAll('rect')
+                .data(function(d) { return d.ys; })
+             .enter().append('rect')
+                .attr('width', x.rangeBand())
+                .attr('height', function(d) { return y(d.y0) - y(d.y1); })
+                .attr('y', function(d) { return y(d.y1); })
+                .attr('class', function(d) { return d['class']; });
+
+        // Plot axes
+        svg.append('g')
+           .attr('class', 'x axis')
+           .attr('transform', 'translate(' + opts.inner_margin.left + ',' +
+                (height + opts.inner_margin.top + opts.inner_margin.bottom) + ')')
+           .call(x_axis)
+           .selectAll('text')
+                .style('text-anchor', 'end')
+                .style('transform', 'rotate(-45deg)')
+                .attr('dx', '-.5em')
+                .attr('dy', '.5em');
+
+        svg.append('g')
+           .attr('class', 'y axis')
+           .attr('transform', 'translate(0,' + opts.inner_margin.top + ')')
+           .call(y_axis);
+    };
+
     // Expose a lightweight public interface
     user_stats.badge_set_milestones = [];
     user_stats.answer_counts = [];
@@ -117,6 +194,19 @@
         preprocess();
         render_milestones({
             container: '#stats-milestones',
+            width:     700,
+            height:    300,
+            margin:    { top:    20,
+                         right:  20,
+                         bottom: 80,
+                         left:   50 },
+            inner_margin: { top:    0,
+                            right:  0,
+                            bottom: 0,
+                            left:   10 }
+            });
+        render_answers({
+            container: '#stats-answers',
             width:     700,
             height:    300,
             margin:    { top:    20,
