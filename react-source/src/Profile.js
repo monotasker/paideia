9300734a0ce4c50b9a3b583140cd9b7bc75bc85d
Ian W. Scott <scottianw@gmail.com>
2019-03-05 17:51:21 -0500

    Setting up basic front-end ui with react-router for navigation and bootstrap.

---
+++ react-source/src/Profile.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+class Profile extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Profile</h2>
+        <p>Mauris sem velit, vehicula eget sodales vitae,
+        rhoncus eget sapien:</p>
+        <ol>
+          <li>Nulla pulvinar diam</li>
+          <li>Facilisis bibendum</li>
+          <li>Vestibulum vulputate</li>
+          <li>Eget erat</li>
+          <li>Id porttitor</li>
+        </ol>
+      </div>
+    );
+  }
+}
+
+export default Profile;
