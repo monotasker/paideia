ec3f29d90f06c98137a2db460d6ed7ee24aa631e
Ian W. Scott <scottianw@gmail.com>
2019-03-26 11:31:02 -0400

    svg manipulation service now converted to js and partially working.

--- static/react-source/src/Services/mapNavService.js
+++ static/react-source/src/Services/mapNavService.js
@@ -1,26 +1,27 @@
 'use strict';
 
-import { velocity } from "velocity";
+import Velocity from "velocity-animate";
+import svgPanZoom from "svg-pan-zoom";
 
 function show_mask() {
     // ========= hide white mask when map is loaded ========== //
-  document.querySelector('div#exploring-mask')
-    .velocity({opacity: "1"}, {display: "block"}, 200);
+  let $mask = document.querySelector('div#exploring-mask');
+  Velocity($mask, {opacity: "1"}, {display: "block"}, 200);
 }
 
 function hide_mask() {
     // ========= hide white mask when map is loaded ========== //
-  document.querySelector('div#exploring-mask')
-    .velocity({opacity: "0"}, {display: "none"}, 700);
+  let $mask = document.querySelector('div#exploring-mask');
+  Velocity($mask, {opacity: "0"}, {display: "none"}, 700);
 }
 
 function fit_to_height() {
     // Handle page dimensions and spinner position
     const headroom = document.querySelector('.navbar').offsetHeight;
     const divheight = window.clientHeight - headroom;
-    let $vertElems = window.querySelectorAll('#page, .speaker img, #town_map')
-    $vertElems.forEach(function (elem) {
-        elem.height(divheight);
+    let $vertElems = document.querySelectorAll('#page, .speaker img, #town_map');
+    $vertElems.forEach((elem) => {
+        elem.style.height = divheight;
     });
     document.querySelector('#town_map').style.width = '100%';
     var $spinners = document.querySelectorAll('#loading-mask img, #exploring-mask img');
@@ -33,49 +34,48 @@ function svg_go_there(evt){
     // show_mask();
     let $p = window.parent.document;
     let $mask = $p.querySelector('#exploring-mask');
-    $mask.style.display = "block";
-    $mask.style.opacity = "0";
-    $mask.velocity({opacity: '1'});
+    $mask.style.cssText = "display: block; opacity: 0;";
+    Velocity($mask, {opacity: '1'});
     let oname = evt.target.id;
-    window.parent.location("/walk/" + oname);
+    window.parent.location = "/walk/" + oname;
 }
 
 function svg_mask_other(evt){
     let $tobj = evt.target;
-    let $svg = $tobj.parents('svg');
-    $svg.find('#' + $tobj.attr('id') + '_mask')
-        .css({display: 'inline', opacity: '0'})
-        .animate({opacity: '0.4'}, 800);
-    $svg.find('#' + $tobj.attr('id') + '_tip')
-        .css({display: 'inline', opacity: '0'})
-        .animate({opacity: '1'}, 300);
+    let $svg = $tobj.closest('svg');
+    let $mask = $svg.querySelector('#' + $tobj.id + '_mask');
+    $mask.style.cssText = "display: inline; opacity: 0;";
+    Velocity($mask, {opacity: '0.4'}, 800);
+    let $tip = $svg.querySelector('#' + $tobj.id + '_tip');
+    $tip.style.cssText = "display: inline; opacity: 0;";
+    Velocity($tip, {opacity: '1'}, 300);
 }
 
 function svg_show_other(evt){
-    var $tobj = $(evt.currentTarget);
-    var $svg = $tobj.parents('svg');
-    $svg.find('#' + $tobj.attr('id') + '_mask')
-        .css({display: 'none'}, {opacity: '0'});
-    $svg.find('#' + $tobj.attr('id') + '_tip')
-        .css({display: 'none'}, {opacity: '0'});
+    var $tobj = evt.target;
+    var $svg = $tobj.closest('svg');
+    let $mask = $svg.querySelector('#' + $tobj.id + '_mask');
+    $mask.style.cssText = "display: 'none'; opacity: 0;";
+    let $tip = $svg.querySelector('#' + $tobj.id + '_tip');
+    $tip.style.cssText = "display: none; opacity: 0;";
 }
 
 function svg_show_guide(evt){
-    $(evt.currentTarget).parents('svg').find('#hotspot_guide')
-        .css({display: 'inline', opacity: '0'})
-        .animate({opacity: '1'}, 300);
+    let $guide = evt.target.closest('svg').querySelector('#hotspot_guide');
+    $guide.style.cssText = "display: inline; opacity: 0;";
+    Velocity($guide, {opacity: '1'}, 300);
 }
 
 function svg_hide_guide(evt){
-    $(evt.currentTarget).parents('svg').find('#hotspot_guide')
-        .animate({opacity: '0'}, 500, function(){
-            $(this).css({display: 'none'});
-        });
+    let $guide = evt.target.closest('svg').querySelector('#hotspot_guide');
+    Velocity($guide, {opacity: '0'}, 500, function(me){
+        me.display = 'none';
+    });
 }
 
 function svg_check_guide_vis(evt){
-    var myguide = $(evt.currentTarget).parents('svg').find('#hotspot_guide');
-    if ( myguide.css('opacity') == '0' ) {
+    let $guide = evt.target.closest('svg').querySelector('#hotspot_guide');
+    if ( $guide.style.opacity == '0' ) {
         return false;
     } else {
         return true;
@@ -83,56 +83,51 @@ function svg_check_guide_vis(evt){
 }
 
 function set_svg_interactions(select_string) {
-    var mysvg = $('#' + select_string)[0].contentDocument;
-    var $mysvg = $(mysvg);
-    var locs = '#domus_A, #bath, #ne_stoa, #agora, #gymnasion, #synagogue';
-    $mysvg.find(locs).each(function(){
-        $(this).on('mouseenter touchstart', svg_mask_other)
-            .on('mouseleave', svg_show_other)
-            .on('click touchstart', svg_go_there);
-            // .on('mousedown touchstart', function( e ) {
-            //   e.stopImmediatePropagation();
-            // });
+    let $svg = document.querySelector('#' + select_string).contentDocument;
+    const locs = '#domus_A, #bath, #ne_stoa, #agora, #gymnasion, #synagogue';
+    let $locs = $svg.querySelectorAll(locs);
+    $locs.forEach(function(loc){
+        loc.addEventListener('mouseenter', svg_mask_other);
+        loc.addEventListener('touchstart', svg_mask_other);
+        loc.addEventListener('mouseleave', svg_show_other);
+        loc.addEventListener('click', svg_go_there);
+        loc.addEventListener('touchstart', svg_go_there);
     });
     //set up revealing of hotspot guide
-    $mysvg.find('#hotspot_guide_trigger')
-        .on('mouseenter', svg_show_guide)
-        .on('mouseleave', svg_hide_guide)
-        .on('touchstart', function(e) {
-            if ( svg_check_guide_vis(e) === false ) {
-                svg_show_guide(e);
-            } else {
-                svg_hide_guide(e);
-            }
-        });
+    let $guide = $svg.querySelector('#hotspot_guide_trigger');
+    $guide.addEventListener('mouseenter', svg_show_guide);
+    $guide.addEventListener('mouseleave', svg_hide_guide);
+    $guide.addEventListener('touchstart', (e) => {
+      if ( svg_check_guide_vis(e) === false ) {
+          svg_show_guide(e);
+      } else {
+          svg_hide_guide(e);
+      }
+    });
 }
 
 function make_map_pan(select_string) {
     // Set svg map up to pan and scroll
     let mapelem = document.getElementById(select_string);
-    mapelem.onload = function(){
+    mapelem.onload = () => {
         let mymap = null;
-        const beforePan = function(oldPan, newPan) {
+        const beforePan = (oldPan, newPan) => {
             const sizes = mymap.getSizes();
-
             // Multiply viewbox width of SVG by current zoom and subtract
             // viewport width to get amount of overflown SVG.
             const x = -((sizes.viewBox.width * sizes.realZoom) - sizes.width);
-
             // Multiply viewbox height of SVG by current zoom and subtract
             // viewport height to get amount of overflown SVG.
             const y = -((sizes.viewBox.height * sizes.realZoom) - sizes.height);
-
             // Determine what’s lower, the current drag x/y coords or zero
             const minx = Math.min(0, newPan.x);
             const miny = Math.min(0, newPan.y);
-
             return {
                 x: Math.max(x, minx),
                 y: Math.max(y, miny)
             };
         };
-        var mapInit = function(){
+        const mapInit = () => {
             mymap = svgPanZoom('#' + select_string, {
                                      beforePan,
                                      zoomEnabled: true,
@@ -144,8 +139,8 @@ function make_map_pan(select_string) {
         }
         mapInit();
         hide_mask();
-        set_svg_interactions('town_map');
+        set_svg_interactions(select_string);
     }
 }
 
-export {};
+export { make_map_pan };
