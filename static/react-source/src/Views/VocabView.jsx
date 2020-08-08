2497c4c34215aae0d648f299f2984ae6acd178da
Ian W. Scott <scottianw@gmail.com>
2020-08-07 20:49:54 -0400

    Vocab interface working and largely polished; Still needs work for narrow widths

--- static/react-source/src/Views/VocabView.jsx
+++ static/react-source/src/Views/VocabView.jsx
@@ -1,20 +1,25 @@
 import React, { useEffect, useState, useMemo, useContext, useRef, useLayoutEffect } from "react";
 import {
-    Row,
-    Col,
-    Table,
-    Form,
-    Button,
-    OverlayTrigger,
-    Tooltip,
     Badge,
-    Spinner
+    Button,
+    Col,
+    Collapse,
+    Form,
+    OverlayTrigger,
+    Row,
+    Spinner,
+    Table,
+    ToggleButton,
+    ToggleButtonGroup,
+    Tooltip,
 } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import greekUtils from 'greek-utils/lib/index'
 
 import { fetchVocabulary } from "../Services/stepFetchService";
 import { UserContext } from "../UserContext/UserProvider";
+import { urlBase } from "../variables";
 
 
 const LinkHeading = ({ field, label, mySortCol, myOrder, sortHandler }) => {
@@ -43,7 +48,52 @@ const LinkHeading = ({ field, label, mySortCol, myOrder, sortHandler }) => {
 }
 
 
+const PrincipalParts = ({w}) => {
+  let pp = null;
+  const principal_parts = [{tense: "future active", form: w['future']},
+                             {tense: "aorist active", form: w['aorist_active']},
+                             {tense: "perfect active", form: w['perfect_active']},
+                             {tense: "aorist passive", form: w['aorist_passive']},
+                             {tense: "perfect passive", form: w['perfect_passive']}
+                            ];
+  if ( principal_parts.some(i => !["", null, "none", undefined].includes(i.form)) ) {
+    const myPP = principal_parts.map(p =>
+      <li key={`${w.id}_${p.tense}`}>
+      <OverlayTrigger placement="top"
+        overlay={
+          <Tooltip id={`tooltip-${w.id}_${p.tense}`}>
+            {!["", null, "none", undefined, "--"].includes(p.form) ?
+              `${p.tense} indicative (1p sing.) form of ${w.accented_lemma}` : `${w.accented_lemma} does not appear in the ${p.tense} indicative`}
+          </Tooltip>
+        }
+      >
+        <a className={`vocab keyforms verb ${p.tense}`}>
+          {!["", null, "none", undefined].includes(p.form) ? <span className={`${p.tense}`}>{p.form}</span> : "--"}
+        </a>
+      </OverlayTrigger>
+      </li>
+      );
+    pp = (
+      <div className="principal-parts">
+        <OverlayTrigger placement="top"
+          overlay={
+            <Tooltip id={`tooltip-pp-label-${w.id}`}>Principal parts: Hover over a form for an explanation</Tooltip>
+          }
+        >
+          <Badge pill className="label">PP</Badge>
+        </OverlayTrigger>
+        <ul>{myPP}</ul>
+      </div>
+    );
+      }
+  else {
+    pp = null;
+  }
+  return pp;
+}
+
 const WordRow = ({ w }) => {
+  const [ showDetails, setShowDetails ] = useState(false);
   const parts_of_speech = {
     noun: 'N',
     proper_noun: 'PN',
@@ -57,18 +107,67 @@ const WordRow = ({ w }) => {
   console.log('word row rendering');
   return (
     <tr>
-      <td className={w.part_of_speech}>{w.accented_lemma}</td>
+      <td className={w.part_of_speech}>
+        {w.accented_lemma}
+        <span className="vocab-videos">{w.videos.map((v, i) => (
+            <OverlayTrigger key={i} placement="top"
+              overlay={
+                <Tooltip id={`tooltip-video-${w.id}`}>{v[1]}</Tooltip>
+              }
+            >
+              <LinkContainer to={`/${urlBase}/videos/${v[0]}`}>
+                <FontAwesomeIcon icon="video" />
+              </LinkContainer>
+            </OverlayTrigger>
+          ))}
+        </span>
+        {!!w.real_stem && w.real_stem != 'none' ?
+          <span className="real-stem">
+            <OverlayTrigger placement="top"
+              overlay={
+                <Tooltip id={`tooltip-stem-${w.id}`}>Real stem</Tooltip>
+              }
+            >
+              <span className="label"><FontAwesomeIcon icon="seedling" /></span>
+            </OverlayTrigger>
+            {w.real_stem}</span>
+          : ''}
+        {!!w.genitive_singular && w.genitive_singular != 'none' ?
+          <span className="gen-sing">
+            <OverlayTrigger placement="top"
+              overlay={
+                <Tooltip id={`tooltip-gen-sing-${w.id}`}>Genitive singular form</Tooltip>
+              }
+            >
+              <Badge pill className="label">G</Badge>
+            </OverlayTrigger>
+            {w.genitive_singular}</span>
+          : ''}
+        <PrincipalParts w={w} />
+        {!["", null, "none", undefined].includes(w['other_irregular']) ?
+          <span className="other-irregular">
+            <a className="label"
+              onClick={() => setShowDetails(!showDetails)}
+              aria-controls="other-irregular-content"
+              aria-expanded={showDetails}
+            >
+              <FontAwesomeIcon icon="caret-down" />{!showDetails ? "show more" : "hide"} details
+            </a>
+            <Collapse in={showDetails}>
+              <div className="other-irregular-content">{w.other_irregular}</div>
+            </Collapse>
+          </span>
+          : ''}
+      </td>
       <td className={w.part_of_speech}><Badge pill>{parts_of_speech[w.part_of_speech]}</Badge></td>
       <td><ul>{w.glosses.map((g, i) => <li key={i}>{g}</li>)}</ul></td>
-      <td>{w.key_forms}</td>
       <td>{w.set_introduced}</td>
-      <td><ul>{w.videos.map((v, i) => <li key={i}><a href={v[2]}>{v[1]}</a></li>)}</ul></td>
       <td>{w.times_in_nt}</td>
     </tr>
   )
 }
 
-
+// FIXME: Don't fire when pane just flies in!
 const vocabIsEqual = (prevProps, nextProps) => {
   console.log('checking vocab equality');
   const oldWordIDs = JSON.stringify(prevProps.vocab.map(w => w.id));
@@ -94,7 +193,7 @@ const VocabTable = React.memo(({ headings, vocab, sortCol, order, sortHandler })
       <tbody>
         {vocab.map((w, i) => <WordRow w={w} key={`wordrow_${i}`} />)}
         <tr className="scrollDownTarget">
-          {headings.map(() => <td></td>)}
+          {headings.map(({label, field}) => <td key={label}></td>)}
         </tr>
       </tbody>
     </Table>
@@ -115,15 +214,23 @@ const VocabView = (props) => {
     return myCol != "" ? [...words].sort(compareVocab) : words
   }
 
-  const doVocabFilter = (vocabIn, strGk, strEng) => {
+  const doVocabFilter = (vocabIn, searchLetters, strGk, strEng) => {
     console.log(vocabIn.length);
+    console.log(searchLetters);
 
-    let gkSetFinal = vocabIn;
+    let baseVocab = vocabIn;
+    if ( searchLetters.length ) {
+      baseVocab = vocabIn.filter(w => {
+        return searchLetters.includes(greekUtils.sanitizeDiacritics(w.normalized_lemma[0]).toUpperCase());
+      });
+    }
+
+    let gkSetFinal = baseVocab;
     if ( strGk != "" ) {
       const strGkSan = greekUtils.sanitizeDiacritics(strGk);
-      const gkSetMatched = vocabIn.filter(w => w.normalized_lemma == strGkSan);
+      const gkSetMatched = baseVocab.filter(w => w.normalized_lemma == strGkSan);
       console.log(gkSetMatched.length);
-      const gkSetFuzzy = vocabIn.filter(
+      const gkSetFuzzy = baseVocab.filter(
         w => w.normalized_lemma != strGkSan && w.normalized_lemma.includes(strGkSan)
       );
       gkSetFinal = gkSetMatched.concat(gkSetFuzzy);
@@ -156,9 +263,10 @@ const VocabView = (props) => {
   );
   const [ searchString, setSearchString ] = useState("");
   const [ searchStringEng, setSearchStringEng ] = useState("");
+  const [ searchLetters, setSearchLetters ] = useState([]);
   const filteredVocab = useMemo(
-    () => doVocabFilter(sortedVocab, searchString, searchStringEng),
-    [sortedVocab, searchString, searchStringEng]
+    () => doVocabFilter(sortedVocab, searchLetters, searchString, searchStringEng),
+    [sortedVocab, searchLetters, searchString, searchStringEng]
   );
   const [ chosenSets, setChosenSets ] = useState(
     Array.from('x'.repeat(user.currentBadgeSet), (_, i) => 1 + i)
@@ -245,63 +353,12 @@ const VocabView = (props) => {
 
     const newVocab = vocab.map((i) => {
       return {...i,
-              normalized_lemma: greekUtils.sanitizeDiacritics(i['normalized_lemma']),
-              key_forms: makeForms(i)
+              normalized_lemma: greekUtils.sanitizeDiacritics(i['normalized_lemma'])
       }
     });
     return newVocab
   }
 
-  const makeForms = (w) => {
-    console.log('making forms');
-    const forms = [];
-    if ( !["", null, "none", undefined].includes(w['real_stem']) ) {
-      forms.push(<span key="realstem" className='vocab keyforms realstem'>
-                  {`real stem: ${w['real_stem']}`}
-                 </span>);
-    }
-    if ( !["", null, "none", undefined].includes(w['genitive_singular']) ) {
-      forms.push(<span key="gen" className='vocab keyforms genitive'>
-                  {`gen: ${w['genitive_singular']};`}
-                 </span>);
-    }
-    const principal_parts = [{tense: "future active", form: w['future']},
-                             {tense: "aorist active", form: w['aorist_active']},
-                             {tense: "perfect active", form: w['perfect_active']},
-                             {tense: "aorist passive", form: w['aorist_passive']},
-                             {tense: "perfect passive", form: w['perfect_passive']}
-                            ];
-    if ( principal_parts.some(i => !["", null, "none", undefined].includes(i.form)) ) {
-
-      const myPP = principal_parts.map(p =>
-        <li key={`${w.id}_${p.tense}`}>
-        <OverlayTrigger placement="top"
-          overlay={
-            <Tooltip id={`tooltip-${w.id}_${p.tense}`}>
-              {!["", null, "none", undefined].includes(p.form) ?
-                `${p.tense} indicative (1p sing.) form of ${w.accented_lemma}` : `${w.accented_lemma} does not appear in the ${p.tense} indicative`}
-            </Tooltip>
-          }
-        >
-          <a className={`vocab keyforms verb ${p.tense}`}>
-           {!["", null, "none", undefined].includes(p.form) ? <span className={`${p.tense}`}>{p.form} </span> : "--"}
-          </a>
-        </OverlayTrigger>
-        </li>
-        );
-      forms.push(<div key="pps" className="vocab keyforms pps">
-                  principal parts:<ul>{myPP}</ul>
-                 </div>);
-    }
-    if ( !["", null, "none", undefined].includes(w['other_irregular']) ) {
-      forms.push(<div key="other" className="vocab keyforms other">
-                  {`other irregular forms: ${w['other_irregular']}`}
-                 </div>);
-    }
-    // console.log(forms);
-
-    return forms;
-  }
 
   const resetAction = () => {
     setSearchString("");
@@ -311,6 +368,7 @@ const VocabView = (props) => {
     document.getElementById('vocab-search-control-english').value = "";
     document.getElementById('vocab-set-control').value = "all badge sets";
     setDisplayRange([0, 20]);
+    setSearchLetters([]);
   }
 
   const restrictSetsAction = (myString) => {
@@ -327,18 +385,14 @@ const VocabView = (props) => {
   }
 
   const headings = [
-    {label: "Dictionary form",
+    {label: "Greek word",
      field: "normalized_lemma"},
     {label: "PoS",
      field: "part_of_speech"},
     {label: "Glosses",
      field: null},
-    {label: "Key forms",
-     field: null},
     {label: "Set",
      field: "set_introduced"},
-    {label: "Video",
-     field: null},
     {label: "In NT",
      field: "times_in_nt"}
   ]
@@ -347,35 +401,35 @@ const VocabView = (props) => {
      <Row key="VocabView" className="vocabview-component panel-view">
          <Col>
             <h2>Vocabulary</h2>
-            <Form>
+            <Form className="vocab-filter-form">
               <Form.Row>
-                <Col>
+                <Col xs="3">
                   <Form.Group
                     controlId="vocab-search-control"
                     onChange={e => setSearchString(e.target.value)}
                   >
-                    {/* <Form.Label><FontAwesomeIcon icon="search" />Search</Form.Label> */}
-                    <Form.Control placeholder="Search Greek words"
-                    ></Form.Control>
+                    <Form.Label><FontAwesomeIcon icon="search" />Greek</Form.Label>
+                    <Form.Control placeholder=""></Form.Control>
                   </Form.Group>
                 </Col>
-                <Col>
+                <Col xs="3">
                   <Form.Group
                     controlId="vocab-search-control-english"
                     onChange={e => setSearchStringEng(e.target.value)}
                   >
-                    {/* <Form.Label><FontAwesomeIcon icon="search" />Search</Form.Label> */}
-                    <Form.Control placeholder="Search English glosses"
-                    ></Form.Control>
+                    <Form.Label><FontAwesomeIcon icon="search" />English</Form.Label>
+                    <Form.Control placeholder=""></Form.Control>
                   </Form.Group>
                 </Col>
-                <Col>
+                <Col xs="4">
                   <Form.Group controlId="vocab-set-control">
-                    {/* <Form.Label><FontAwesomeIcon icon="filter" />Badge set</Form.Label> */}
+                    <Form.Label><FontAwesomeIcon icon="filter" />Badge sets</Form.Label>
                     <Form.Control as="select"
                       onChange={e => restrictSetsAction(e.target.value)}
                     >
-                      <option key="0">{`sets 1 to ${user.currentBadgeSet}`}</option>
+                      {!!user.currentBadgeSet &&
+                        <option key="0">{`sets 1 to ${user.currentBadgeSet}`}</option>
+                      }
                       <option key="1">all badge sets</option>
                       {Array.from('x'.repeat(20), (_, i) => 1 + i).map( n =>
                           <option key={n + 1}>{`set ${n}`}</option>
@@ -383,12 +437,26 @@ const VocabView = (props) => {
                     </Form.Control>
                   </Form.Group>
                 </Col>
-                <Col>
+                <Col xs="2">
                   <Button onClick={() => resetAction()}
                   >
                     <FontAwesomeIcon icon="redo-alt" />
-                    Clear
+                    <span className="d-none d-xl-inline">Clear</span>
                   </Button>
+                </Col>
+              </Form.Row>
+              <Form.Row className="alphabet">
+                <Col>
+                  <ToggleButtonGroup type="checkbox" size="sm"
+                    value={searchLetters}
+                    onChange={val => setSearchLetters(val)}
+                  >
+                    {['Α', 'Β', 'Γ', 'Δ', 'Ε', 'Ζ', 'Η', 'Θ', 'Ι', 'Κ', 'Λ', 'Μ', 'Ν', 'Ξ', 'Ο', 'Π', 'Ρ', 'Σ', 'Τ', 'Υ',  'Φ', 'Χ', 'Ψ', 'Ω'].map( l =>
+                      <ToggleButton variant="outline-secondary"
+                        value={l} key={l}
+                      >{l}</ToggleButton>
+                    )}
+                  </ToggleButtonGroup>
                 </Col>
               </Form.Row>
             </Form>
